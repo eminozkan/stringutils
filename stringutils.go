49b3dd3d38a569238e3794b52124698b3076c18a
meozkan <info@eminozkan.net>
2023-08-31 11:21:48 +0300

    add reverse function

---
+++ stringutils.go
@@ -0,0 +1,24 @@
+package stringutils
+
+import (
+	"errors"
+	"unicode/utf8"
+)
+
+var ErrInvalidUTF8 = errors.New("input is not a valid UTF-8")
+
+// Reverse reverses given string!
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, ErrInvalidUTF8
+	}
+	r := []rune(s)
+	lr := len(r)
+	ss := make([]rune, lr)
+
+	for i := 0; i < lr; i++ {
+		ss[lr-1-i] = r[i]
+	}
+
+	return string(ss), nil
+}
